bc951c91b3a89a1550d2801b23b8126d7b3964cb
Osman <osman_saeed@hotmail.co.uk>
2022-07-12 11:33:07 +0100

    mocking weather

--- airport.js
+++ airport.js
@@ -1,7 +1,8 @@
 class Airport {
-  constructor (capacity = 10) {
+  constructor (weatherGenerator, capacity = 10) {
     this.capacity = capacity;
     this.slots = []
+    this.weather = weatherGenerator;
   }
   showCapacity() {
     return this.capacity;
@@ -11,7 +12,16 @@ class Airport {
     return this.slots
   }
   land(plane) {
-    this.slots.push(plane)
+    if( this.weather.current() > 0.1) {
+      if(this.slots.length < this.capacity) {
+        this.slots.push(plane)
+      } else {
+        throw('Airport is full');
+      }
+    } else {
+      throw('Weather is too bad');
+    }
+
   }
   takeoff(plane) {
     this.slots = this.slots.filter(element => (element != plane) )
--- airport.test.js
+++ airport.test.js
@@ -3,13 +3,15 @@ const Airport = require('./airport');
 describe('Airport', () => {
   describe('initiaslisation', () => {
     it('inistiaslies with zero plane and a default capacity', () => {
-      const airport = new Airport();
+      const mockWeather = {};
+      const airport = new Airport(mockWeather);
       expect(airport.showCurrentPlanes()).toStrictEqual([]) // let's assume the output is an array first
       expect(airport.showCapacity()).toBe(10)
       expect(airport.showEmptySlot()).toBe(10)
     });
     it('initialises with zero plane but a given capactiy', () => {
-      const airport = new Airport(15);
+      const mockWeather = {};
+      const airport = new Airport(mockWeather, 15);
       expect(airport.showCurrentPlanes()).toStrictEqual([]) // let's assume the output is an array first
       expect(airport.showCapacity()).toBe(15)
       expect(airport.showEmptySlot()).toBe(15)
@@ -17,7 +19,10 @@ describe('Airport', () => {
   });
   describe('landing a plane', () => {
     it('lands a plane', () => {
-      const airport = new Airport();
+      const mockWeather = {
+        current: () => 0.8
+      };
+      const airport = new Airport(mockWeather);
       const mockPlane = {};
       airport.land(mockPlane);
       expect(airport.showCurrentPlanes()).toStrictEqual([mockPlane]);
@@ -25,7 +30,10 @@ describe('Airport', () => {
       expect(airport.showEmptySlot()).toBe(9)
     });
     it('lands multiple planes', () => {
-      const airport = new Airport();
+      const mockWeather = {
+        current: () => 0.8
+      };
+      const airport = new Airport(mockWeather);
       const mockPlane = {};
       const mockPlane2 = {};
       airport.land(mockPlane);
@@ -34,10 +42,27 @@ describe('Airport', () => {
       expect(airport.showCapacity()).toBe(10)
       expect(airport.showEmptySlot()).toBe(8)
     });
+    it('prevents planes from landing when airport is full', () => {
+      const mockWeather = {
+        current: () => 0.8
+      };
+      const airport = new Airport(mockWeather);
+      const mockPlane = {};
+      for(let i = 1; i <= 10; i++){
+        airport.land(mockPlane);
+      }
+      expect(airport.showEmptySlot()).toBe(0);
+      expect(() => {
+        airport.land(mockPlane);
+      }).toThrow('Airport is full');
+    })
   });
   describe('planes taking off', () => {
     it('instructs a plane to take off', () => {
-      const airport = new Airport();
+      const mockWeather = {
+        current: () => 0.8
+      };
+      const airport = new Airport(mockWeather);
       const mockPlane = {};
       //landing a plane
       airport.land(mockPlane);
@@ -49,7 +74,10 @@ describe('Airport', () => {
       expect(airport.showEmptySlot()).toBe(10)
     });
     it('instructs a plane to take off', () => {
-      const airport = new Airport();
+      const mockWeather = {
+        current: () => 0.8
+      };
+      const airport = new Airport(mockWeather);
       const mockPlane = {};
       const mockPlane2 = {};
       //landing a plane
